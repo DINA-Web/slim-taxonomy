3cf95a377d2fa05d60c26c2340e10bde8f64797c
Mikko Heikkinen <hawk@biomi.org>
2017-11-01 13:06:42 +0100

    Refactor JSON-API structure generation to a method

--- www/html/app/src/taxon_model.php
+++ www/html/app/src/taxon_model.php
@@ -21,9 +21,35 @@ Class Taxon
         $statement->bindValue(":id", $id, PDO::PARAM_INT);
         $statement->execute();
 
-        $data = $statement->fetch(); // Expecting only one row
+        $taxon = $statement->fetch(); // Expecting only one row
 
-        if ("SPECIES" == $data['TaxonLevel']) {
+        return $this->taxonToJSONAPIArray($taxon, $withParent);
+    }
+
+    public function fetchName($name, $withParent = TRUE) {
+
+        $nameParts = explode(" ", $name);
+
+        $sql = "
+            SELECT *
+            FROM mammal_msw
+            WHERE Genus=:genus
+            AND Species=:species_epithet
+        ";
+        $statement = $this->db->prepare($sql);
+        $statement->bindValue(":genus", $nameParts[0], PDO::PARAM_INT);
+        $statement->bindValue(":species_epithet", $nameParts[1], PDO::PARAM_INT);
+        $statement->execute();
+
+        $taxon = $statement->fetch(); // Expecting only one row
+
+//        exit(print_r($taxon)); // debug
+
+        return $this->taxonToJSONAPIArray($taxon, $withParent);        
+    }
+
+    public function taxonToJSONAPIArray($taxon, $withParent) {
+        if ("SPECIES" == $taxon['TaxonLevel']) {
 
             if ($withParent) {
 
@@ -32,7 +58,7 @@ Class Taxon
                     SELECT *
                     FROM mammal_msw
                     WHERE TaxonLevel = 'GENUS'
-                    AND Genus = '" . $data['Genus'] . "'
+                    AND Genus = '" . $taxon['Genus'] . "'
                 ";
                 $statement = $this->db->prepare($sql);
                 $statement->execute();
@@ -44,36 +70,36 @@ Class Taxon
             }
         
             // Higher taxa
-            $attributes['higherTaxa']['order'] = ucfirst(strtolower($data['Order']));
-            $attributes['higherTaxa']['suborder'] = ucfirst(strtolower($data['Suborder']));
-            $attributes['higherTaxa']['infraorder'] = ucfirst(strtolower($data['Infraorder']));
-            $attributes['higherTaxa']['superfamily'] = $data['Superfamily'];
-            $attributes['higherTaxa']['family'] = $data['Family'];
-            $attributes['higherTaxa']['subfamily'] = $data['Subfamily'];
-            $attributes['higherTaxa']['tribe'] = $data['Tribe'];
-            $attributes['higherTaxa']['genus'] = $data['Genus'];
-            $attributes['higherTaxa']['subgenus'] = $data['Subgenus'];
+            $attributes['higherTaxa']['order'] = ucfirst(strtolower($taxon['Order']));
+            $attributes['higherTaxa']['suborder'] = ucfirst(strtolower($taxon['Suborder']));
+            $attributes['higherTaxa']['infraorder'] = ucfirst(strtolower($taxon['Infraorder']));
+            $attributes['higherTaxa']['superfamily'] = $taxon['Superfamily'];
+            $attributes['higherTaxa']['family'] = $taxon['Family'];
+            $attributes['higherTaxa']['subfamily'] = $taxon['Subfamily'];
+            $attributes['higherTaxa']['tribe'] = $taxon['Tribe'];
+            $attributes['higherTaxa']['genus'] = $taxon['Genus'];
+            $attributes['higherTaxa']['subgenus'] = $taxon['Subgenus'];
 
             // Taxon
-            $attributes['rank'] = strtolower($data['TaxonLevel']);
-            $attributes['scientific_name'] = $data['Genus'] . " " . $data['Species'];
-            $attributes['author'] = $data['Author'];
-            $attributes['author_date'] = $data['AuthorDate'];
+            $attributes['rank'] = strtolower($taxon['TaxonLevel']);
+            $attributes['scientific_name'] = $taxon['Genus'] . " " . $taxon['Species'];
+            $attributes['author'] = $taxon['Author'];
+            $attributes['author_date'] = $taxon['AuthorDate'];
                 
-            if ("YES" == $data['ValidName']) {
+            if ("YES" == $taxon['ValidName']) {
                 $attributes['valid_name'] = TRUE;
             }
             else {
                 $attributes['valid_name'] = FALSE;
             }
 
-            if (!empty($data["CommonName"])) {
-                $attributes['verncular_names']['en'][] = $data["CommonName"];
+            if (!empty($taxon["CommonName"])) {
+                $attributes['verncular_names']['en'][] = $taxon["CommonName"];
             }
 
             // Synonyms
-            if (! empty($data['Synonyms'])) {
-                $synonymsArrHTML = explode(";", $data['Synonyms']);
+            if (! empty($taxon['Synonyms'])) {
+                $synonymsArrHTML = explode(";", $taxon['Synonyms']);
                 $n = 0;
                 foreach($synonymsArrHTML as $key => $synonymHTML) {
                     $synonymArr = explode("</i>", $synonymHTML);
@@ -84,41 +110,17 @@ Class Taxon
             }
 
             // Other data
-            $attributes['sort_order'] = $data['SortOrder'];
+            $attributes['sort_order'] = $taxon['SortOrder'];
             
         }
-    //    $attributes = $data; // debug - see full data from db
+    //    $attributes = $taxon; // debug - see full data from db
 
         $res['jsonapi']['version'] = "1.0";
         $res['meta']['Source'] = "Mammal Species of the World";
         $res['data']['type'] = "taxon";
-        $res['data']['id'] = $id;
-        $res['data']['attributes'] = $attributes;
+        $res['data']['id'] = $taxon['MSW_ID'];
+        $res['data']['attributes'] = $attributes; 
 
         return $res;
     }
-
-    public function fetchName($name) {
-
-        $nameParts = explode(" ", $name);
-
-        $sql = "
-            SELECT MSW_ID
-            FROM mammal_msw
-            WHERE Genus=:genus
-            AND Species=:species_epithet
-        ";
-        $statement = $this->db->prepare($sql);
-        $statement->bindValue(":genus", $nameParts[0], PDO::PARAM_INT);
-        $statement->bindValue(":species_epithet", $nameParts[1], PDO::PARAM_INT);
-        $statement->execute();
-
-        $data = $statement->fetch(); // Expecting only one row
-
-        $taxonData = $this->fetchTaxon($data['MSW_ID']);
-
-        return $taxonData;
-
-    }
-
 }
