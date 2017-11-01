818192dfb34270898a020ee6259c86a1ce840dd7
Mikko Heikkinen <hawk@biomi.org>
2017-11-01 08:23:41 +0100

    Add database connection, fetching single taxon

--- www/html/app/src/routes.php
+++ www/html/app/src/routes.php
@@ -5,26 +5,30 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/taxon/{id}', function (Request $request, Response $response, array $args) {
+    $id = $request->getAttribute('id');
+    $this->logger->info("Route /taxon/$id");
 
-    // Test database
-//    $this->db;
+//    return "Taxon id is " . $id; // debug
+// id 13001562 = Mus musculus
 
-//    phpinfo();
-
-//    $db = $this->getContainer()->get('db'); //  Call to undefined method Slim\Container::getContainer()
     $db = $this->get('db');
-    print_r ($db);
+    $sql = "
+    SELECT *
+    FROM mammal_msw
+    WHERE MSW_ID = $id
+    ";
 
-    if($this->has('db')) {
-        return '$app has db';
-    }
-    else {
-        return '$app doesn\'t have db';
-    }
+    $PDOStatement = $db->query($sql);
+    $data = $PDOStatement->fetch(); // Expecting only one row
 
+    /*
+    while($row = $PDOStatement->fetch()) {
+        $data[] = $row;
+    }
+    */
+
+    print_r ($data);
 
     // Render index view
 //    return $this->renderer->render($response, 'index.phtml', $args);
